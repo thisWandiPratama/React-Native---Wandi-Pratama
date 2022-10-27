7bd0778a9357a93d3414724b2ceed1a7a8b0f60b
thisWandiPratama <wandi99pratama@gmail.com>
2022-10-27 19:29:07 +0700

    update

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,47 +1,40 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-// models
-const db = require('./app/models')
+app.use(bodyParser.json());
 
-const app = express()
-
-let whiteList = [
-    'http://localhost:8081'
-]
-
-let corsOption = {
-    origin: (origin, callback) => {
-        if(whiteList.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    } 
-}
-
-app.use(cors(corsOption))
-
-// parse request application/json x-www-form-urlencode
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// sync database
-db.sequelize.sync()
-
-// simple route
+app.post('/', (req, res) => {
+    if (req.headers.authorization.split()[0] == "ifabula" &&req.headers.authorization.split()[0]=="user") {
+        res.send({
+            responseCode: 401,
+            responseMessage: "UNAUTHORIZED"
+        });
+    } else {
+        res.send({
+            status: 'success',
+            message: 'Test Frontend React Native',
+            method: 'POST',
+            body: req.body
+        });
+    }
+});
 app.get('/', (req, res) => {
-    res.json({
-        message: 'welcome to gjtripay'
-    })
-})
+    if (req.headers.authorization.split()[0] == "ifabula" &&req.headers.authorization.split()[0]=="user") {
+        res.send({
+            responseCode: 401,
+            responseMessage: "UNAUTHORIZED"
+        });
+    } else {
+        res.send({
+            status: 'success',
+            message: 'Test Frontend React Native',
+            method: 'GET',
+        });
+    }
+});
 
-// Posts routes
-require('./app/routes/post.routes')(app)
-
-const PORT = process.env.PORT || 8080
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/`)
-})
+app.listen(port, () => {
+    console.log(`cli-nodejs-api listening at http://localhost:${port}`)
+});
